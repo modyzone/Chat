f00fb3eebd9280e6115b67bbd4a7fde62370fb7d
modyzone <mody.kassem@gmail.com>
2022-01-22 20:26:08 +0100

    add new files

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, View,Text, TextInput, Button, Alert, ScrollView } from 'react-native';
+// import the screens
+import Start from './components/Start';
+import Chat from './components/Chat';
+// import react native gesture handler
+import 'react-native-gesture-handler';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+// import react Navigation
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// Create the navigator
+const Stack = createStackNavigator();
+
+export default class App extends React.Component {
+  // constructor(props) {
+    // super(props);
+    // this.state = { text: '' };
+  // }
+  render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Start">
+          <Stack.Screen name="Start" component={Start} />
+          <Stack.Screen name="Chat" components={Chat} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+  alertMyText(input = []) {
+    Alert.alert(input.text);
+  }
+};
